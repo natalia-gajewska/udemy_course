f611fd4006f1cabf39d93369c8cb24bb91f73f5d
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-09-24 13:53:37 +0200

    zmienne

--- src/pl/nataliagajewska/udemy/firstprogram/Main.java
+++ src/pl/nataliagajewska/udemy/firstprogram/Main.java
@@ -2,7 +2,7 @@ package pl.nataliagajewska.udemy.firstprogram;
 
 //to jest tekst który nic nie wnosi
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //metoda miejsce startu aplikacji
         System.out.println("Hello Intellij");
         /*
         to jest komentarz na linie
---
+++ src/pl/nataliagajewska/udemy/variables/Variables.java
@@ -0,0 +1,28 @@
+package pl.nataliagajewska.udemy.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int x = 52+87; //int - typ zmiennej, x- nazwa zmiennej, = przypisanie
+        byte b = 127; // od 127 do -128
+        short c = Short.MAX_VALUE; // max value stała - 32767 , -32768 (ctrl)
+        long l = Long.MAX_VALUE;
+        float f = Float.MAX_VALUE;
+
+        double d = 0.1; //zmiennoprzecinkowe
+
+        char u = 'm'; //przechowywanie pojedyńczych liter
+
+        String str = "Natalia"; //zmienna tekstowa
+
+        boolean boo = true; //lub false
+
+        System.out.println(str);
+        System.out.println(boo);
+
+
+
+
+    }
+
+}
