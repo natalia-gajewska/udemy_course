136e727570973d41114f73ea0f83930a186fafa6
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-09-24 13:25:24 +0200

    udemy1

---
+++ src/pl/nataliagajewska/udemy/firstprogram/Main.java
@@ -0,0 +1,12 @@
+package pl.nataliagajewska.udemy.firstprogram;
+
+//to jest tekst który nic nie wnosi
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Intellij");
+        /*
+        to jest komentarz na linie
+        lub dwie
+         */
+    }
+}
